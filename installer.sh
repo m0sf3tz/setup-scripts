65ca113ffa2d74714ffa15d847fe96484d4bdc7a
Your Nan <you@exam>
2019-11-05 00:04:13 -0800

    adding go -version, some manuel steps

--- installer.sh
+++ installer.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$EUID" -eq 0 ]
-  then echo "Can't run as root"
-  exit
-fi
-
 cp .vimrc ~/.vimrc
 
 sudo apt-get -y install ctags cscope tmux
@@ -14,3 +9,6 @@ cd ~/.vim/pack/my-pack/start/
 
 git clone https://github.com/fatih/vim-go.git
 git clone https://github.com/majutsushi/tagbar.git
+
+# This will give you the latest version of go
+sudo snap install --classic go
